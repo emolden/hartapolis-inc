5b878bb759335d43e3e908a0df9ba77be291f949
emolden <emolden@travelers.com>
2025-03-18 15:50:01 -0400

    back up to date

--- server/server.js
+++ server/server.js
@@ -1,6 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+const PORT = 3000;
 
-console.log('hello world')
+app.use(express.json());
+app.use(cors());
 
 
-console.log('hartopolis!')
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {console.log(`Server is running on http://localhost:${PORT}`);});
 
