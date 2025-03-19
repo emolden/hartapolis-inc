4022141142e21b4f02ba80b18b935cff99fb037e
Joshua B <jbrozows@email.com>
2025-03-19 10:02:07 -0400

    second end-to-end

--- server/server.js
+++ server/server.js
@@ -1,15 +1,29 @@
 import express from 'express';
 import cors from 'cors';
+import { MongoClient, ObjectId } from 'mongodb';
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(cors());
 
+const MONGO_DB_URL = 'mongodb://localhost:27017';
+const MONGO_DB = 'capstone';
+//const collection = 'projects'
 
-
-
-
+app.get('/', async (req,res) => {
+    try{
+        const client = await MongoClient.connect(MONGO_DB_URL);
+        const db = client.db(MONGO_DB);
+        const collection = db.collection('projects');
+        const projects = await collection.find({}).toArray();
+        res.json(projects);
+    }
+    catch (err) {
+        console.error("Error:", err);
+        return res.status(404).send("Get Did Not Work")
+    }
+})
 
 
 
