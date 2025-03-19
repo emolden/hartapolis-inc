a9231fd381b34ad8e5321914de7ad81130736b70
Joshua B <jbrozows@email.com>
2025-03-19 16:12:03 -0400

    team projects button work

--- react/vite-project/src/components/TeamProjects.jsx
+++ react/vite-project/src/components/TeamProjects.jsx
@@ -5,9 +5,11 @@ const TeamProjects = (props) => {
         <>
             <div>
             <h1>Team Projects</h1>
+            <button>
             {
           props.projects.map((project) => ( <h3>Team Projects {project.project_attributes.name}</h3>))
         }
+            </button>
             </div>
         </>
     );
